c10ac0baa0c23d4f90ef0a78a21259be940ace65
CaioCasemiro <casemirocaio2@gmail.com>
2024-09-13 15:14:54 -0300

    início - atributos feitos

--- ContaBancaria.ts
+++ ContaBancaria.ts
@@ -1,1 +1,15 @@
-export default class ContaBancaria {}
+export default class ContaBancaria {
+        private numeroConta:number;
+        private agencia:number;
+        private saldo:number
+        private extrato:string[]
+
+        constructor(numeroConta:number, agencia: number, saldo: number, extrato:string[]){
+            this.numeroConta = numeroConta
+            this.agencia = agencia
+            this.saldo = saldo
+            this.extrato = extrato
+        }
+
+        
+}
